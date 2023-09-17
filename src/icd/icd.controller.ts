d8a7ac63f385ec7dc9815cc0dbcce39561cc4f02
Ryan Viana <ryanviana@usp.br>
2023-09-17 09:07:21 -0300

    introduz melhorias

--- src/icd/icd.controller.ts
+++ src/icd/icd.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { IcdService } from './icd.service';
 
 @Controller('icd')
 export class IcdController {
   constructor(private readonly icdService: IcdService) {}
 
-  @Get()
+  @Post()
   async getSymptomList(@Body('symptoms') prompt: string) {
     return await this.icdService.getIcdSymptoms(prompt).then((res) => res);
   }
